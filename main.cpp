745d76d57b763c57c6c060741e4be02e061de241
Murilo Carvalho <murilo63793@gmail.com>
2023-05-15 18:31:09 -0300

    refactor: objec3d class load obj files

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
 
 #include "src/Cubemap.h"
 #include "src/camera.h"
+#include "src/object3D.h"
 
 GLuint vao{};
 GLuint vaoPlane{};
@@ -60,22 +61,8 @@ int texWidth{ 500 }, texHeight{ 500 };
 float angleX = 0.90f;
 float angleY = 0.90f;
 
-struct vertex {
-	float x, y, z;
-	float r, g, b;
-};
 
-struct normal {
-	float x, y, z;
-};
-
-struct vertexIndices{
-	int vertex;
-	int normal;
-	int texCoord;
-};
-
-std::vector<vertex> vertices{
+std::vector<Object3D::vertex> vertices{
 	// positions          // colors           
 	{13.5f,  13.5f, 0.0f,   1.0f, 0.0f, 0.0f},   // top right
 	{13.5f, -13.5f, 0.0f,   0.0f, 1.0f, 0.0f },   // bottom right
@@ -83,7 +70,7 @@ std::vector<vertex> vertices{
 	{-13.5f,  13.5f, 0.0f,   1.0f, 1.0f, 0.0f}   // top left 
 };
 
-std::vector<normal> normalsPlane{
+std::vector<Object3D::normal> normalsPlane{
 	// positions          // colors           
 	{0.0f, 0.0f, 1.0f},   // top right
 	{0.0f, 0.0f, 1.0f },   // bottom right
@@ -153,8 +140,8 @@ glm::mat4 rotY = glm::mat4(1.0f);
 
 glm::vec3 lightDir(0.0f, 1.0f, 0.0f);
 
-std::vector<vertex> objectVertices{};
-std::vector<normal> objectNormals{};
+std::vector<Object3D::vertex> objectVertices{};
+std::vector<Object3D::normal> objectNormals{};
 std::vector<glm::vec2> objectTexCoords{};
 
 cy::GLSLProgram program;
@@ -162,6 +149,7 @@ cy::GLSLProgram skyboxProgram;
 
 Cubemap cube;
 Camera cam;
+Object3D object3D;
 
 void myIdle() {
 	glutPostRedisplay();
@@ -356,7 +344,7 @@ void myDisplayTeapot(){
 	glBindTexture(GL_TEXTURE_2D, texID);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 	
-	glDrawElements(GL_TRIANGLES, facesIndex.size(), GL_UNSIGNED_INT, 0);
+	glDrawElements(GL_TRIANGLES, object3D.m_facesIndex.size(), GL_UNSIGNED_INT, 0);
 
 	/*glGenerateTextureMipmap(renderedTexture);
 
@@ -375,8 +363,7 @@ void myDisplayTeapot(){
 	//glDrawArrays(GL_POINTS, 0, objectVertices.size());
 	glutSwapBuffers();
 }
-
-void setObject(){
+void setObject() {
 
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
@@ -384,9 +371,9 @@ void setObject(){
 	//positions
 	glCreateBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glNamedBufferStorage(vbo, objectVertices.size() * sizeof(vertex), objectVertices.data(), 0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)offsetof(vertex, x));
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)offsetof(vertex, r));
+	glNamedBufferStorage(vbo, object3D.m_vertices.size() * sizeof(Object3D::vertex), object3D.m_vertices.data(), 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Object3D::vertex), (void*)offsetof(Object3D::vertex, x));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Object3D::vertex), (void*)offsetof(Object3D::vertex, r));
 
 	glEnableVertexAttribArray(0);//antes de renderizar
 	glEnableVertexAttribArray(1);//antes de renderizar
@@ -394,15 +381,15 @@ void setObject(){
 	//normals
 	glCreateBuffers(1, &vboNormals);
 	glBindBuffer(GL_ARRAY_BUFFER, vboNormals);
-	glNamedBufferStorage(vboNormals, objectNormals.size() * sizeof(normal), objectNormals.data(), 0);
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(normal), 0);
+	glNamedBufferStorage(vboNormals, object3D.m_normals.size() * sizeof(Object3D::normal), object3D.m_normals.data(), 0);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Object3D::normal), 0);
 
 	glEnableVertexAttribArray(2);//antes de renderizar
 
 	//texcoord
 	glCreateBuffers(1, &vboTexCoords);
 	glBindBuffer(GL_ARRAY_BUFFER, vboTexCoords);
-	glNamedBufferStorage(vboTexCoords, objectTexCoords.size() * sizeof(glm::vec2), objectTexCoords.data(), 0);
+	glNamedBufferStorage(vboTexCoords, object3D.m_texCoords.size() * sizeof(glm::vec2), object3D.m_texCoords.data(), 0);
 	glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, sizeof(glm::vec2), 0);
 
 	glEnableVertexAttribArray(3);
@@ -413,7 +400,7 @@ void setObject(){
 	//indices
 	glGenBuffers(1, &ebo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, facesIndex.size() * sizeof(unsigned int), facesIndex.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, object3D.m_facesIndex.size() * sizeof(unsigned int), object3D.m_facesIndex.data(), GL_STATIC_DRAW);
 }
 
 void setFrameBuffer() {
@@ -450,9 +437,9 @@ void setPlane() {
 	//positions
 	glCreateBuffers(1, &vboPlane);
 	glBindBuffer(GL_ARRAY_BUFFER, vboPlane);
-	glNamedBufferStorage(vboPlane, 4 * sizeof(vertex), vertices.data(), 0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)offsetof(vertex, x));
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)offsetof(vertex, r));
+	glNamedBufferStorage(vboPlane, 4 * sizeof(Object3D::vertex), vertices.data(), 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Object3D::vertex), (void*)offsetof(Object3D::vertex, x));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Object3D::vertex), (void*)offsetof(Object3D::vertex, r));
 
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
@@ -460,8 +447,8 @@ void setPlane() {
 	//normals
 	glCreateBuffers(1, &vboNormalsPlane);
 	glBindBuffer(GL_ARRAY_BUFFER, vboNormalsPlane);
-	glNamedBufferStorage(vboNormalsPlane, normalsPlane.size() * sizeof(normal), normalsPlane.data(), 0);
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(normal), 0);
+	glNamedBufferStorage(vboNormalsPlane, normalsPlane.size() * sizeof(Object3D::normal), normalsPlane.data(), 0);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Object3D::normal), 0);
 
 	glEnableVertexAttribArray(2);//antes de renderizar
 
@@ -480,6 +467,7 @@ void setPlane() {
 	setFrameBuffer();
 }
 void setObjectTexture(unsigned width, unsigned height, std::vector<unsigned char> image) {
+	glGenTextures(1, &texCubeID);
 	//texture
 	glBindTexture(GL_TEXTURE_2D, texID);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image.data());
@@ -525,184 +513,6 @@ void setCubemapConfig() {
 	glEnableVertexAttribArray(0);
 }
 
-
-void loadObject(cy::TriMesh mesh, std::vector<vertex>& objectVertices, std::vector<normal>& objectNormals, std::vector<glm::vec2>& objectTexCoords) {
-
-	//vector of vertices
-	int n = mesh.NV();
-	for (int i = 0; i < n; i++) {
-		objectVertices.push_back({ mesh.V(i).x, mesh.V(i).y, mesh.V(i).z, 1.0f, 0.0f , 0.3f });
-	}
-
-	int nf = mesh.NF();
-	int counter{};
-	std::vector<vertexIndices> list{};
-	std::vector<bool> checkVertex(n, false);
-
-	for (int i = 0; i < nf; i++) {
-		for (int k = 0; k < 3; k++) {
-
-			if (mesh.F(i).v[k] >= n || checkVertex[mesh.F(i).v[k]]) {
-				continue;
-			}
-
-			for (int j = i; j < nf; j++) {
-				if (mesh.F(i).v[k] == mesh.F(j).v[k] && (mesh.FN(i).v[k] != mesh.FN(j).v[k])) { //works!
-
-					if (list.empty()) {
-						int vertexI = mesh.F(i).v[k];
-						int normalI = mesh.FN(i).v[k];
-						vertexIndices vertex = { vertexI, normalI };
-						list.push_back(vertex);
-					}
-
-					int vertexI = mesh.F(j).v[k];
-					int normalI = mesh.FN(j).v[k];
-
-					vertexIndices vertex = { vertexI, normalI };
-					auto it = std::find_if(list.begin(), list.end(), [&vertex](const vertexIndices& e) {
-						return e.vertex == vertex.vertex && e.normal == vertex.normal;
-						});
-
-					bool notFoundVertex = it == list.end();
-
-					if (notFoundVertex) {
-						list.push_back(vertex);
-					}
-				}
-			}
-
-		}
-		//se achou vértice com normal distinta
-		if (!list.empty()) {
-
-			//atualiza o valor no vetor de vértice que ele ja foi checado
-			checkVertex[list[0].vertex] = true;
-
-			//adiciona ao vbo de vértices no final o vértices que precisa duplicar
-
-			std::for_each(list.begin() + 1, list.end(), [&mesh, &nf, &objectVertices](vertexIndices x) {
-
-				int tSize = objectVertices.size();
-				//adiciona vertice
-				objectVertices.push_back({ mesh.V(x.vertex).x, mesh.V(x.vertex).y, mesh.V(x.vertex).z, 0.5f, 0.5f, 0.5f });
-
-				//atualiza os indices dos vértices duplicados
-				for (int f = 0; f < nf; f++) {
-					for (int k = 0; k < 3; k++) {
-						if (x.vertex == mesh.F(f).v[k] && x.normal == mesh.FN(f).v[k]) {
-							mesh.F(f).v[k] = tSize;
-						}
-					}
-				}
-				});
-
-			list.clear();
-		}
-
-	}
-
-	int nNormals = mesh.NVN();
-	int normalIndex{};
-
-	for (int i = 0; i < nf; i++) {
-		for (int k = 0; k < 3; k++) {
-
-			if (mesh.F(i).v[k] >= n || checkVertex[mesh.F(i).v[k]]) {
-				continue;
-			}
-
-			for (int j = i; j < nf; j++) {
-				if (mesh.F(i).v[k] == mesh.F(j).v[k] && mesh.FT(i).v[k] != mesh.FT(j).v[k]) { //works!
-					if (list.empty()) {
-						int vertexI = mesh.F(i).v[k];
-						int normalI = mesh.FN(i).v[k];
-						int texI = mesh.FT(i).v[k];
-						vertexIndices vertex = { vertexI, normalI, texI };
-						list.push_back(vertex);
-					}
-
-					int vertexI = mesh.F(j).v[k];
-					int normalI = mesh.FN(j).v[k];
-					int texI = mesh.FT(i).v[k];
-
-					vertexIndices vertex = { vertexI, normalI, texI };
-					auto it = std::find_if(list.begin(), list.end(), [&vertex](const vertexIndices& e) {
-						return e.vertex == vertex.vertex && e.normal == vertex.normal && e.texCoord == vertex.texCoord;
-						});
-
-					bool notFoundVertex = it == list.end();
-
-					if (notFoundVertex) {
-						list.push_back(vertex);
-					}
-				}
-			}
-
-		}
-		//se achou vértice com normal distinta
-		if (list.size() > 0) {
-
-			//atualiza o valor no vetor de vértice que ele ja foi checado
-			checkVertex[list[0].vertex] = true;
-
-			//adiciona ao vbo de vértices no final o vértices que precisa duplicar
-			std::for_each(list.begin() + 1, list.end(), [&mesh, &nf, &objectVertices](vertexIndices x) {
-
-				int tSize = objectVertices.size();
-				//adiciona vertice
-				objectVertices.push_back({ mesh.V(x.vertex).x, mesh.V(x.vertex).y, mesh.V(x.vertex).z, 0.5f, 0.5f, 0.5f });
-				//normals.push_back({ mesh.VN(x.normal).x, mesh.VN(x.normal).y, mesh.VN(x.normal).z});
-
-
-				for (int f = 0; f < nf; f++) {
-					for (int k = 0; k < 3; k++) {
-						//mesh.VN(mesh.FN(f).v[k]).x
-						if (x.vertex == mesh.F(f).v[k] && x.normal == mesh.FN(f).v[k] && x.texCoord == mesh.FT(f).v[k]) {
-							mesh.F(f).v[k] = tSize;
-							mesh.FN(f).v[k] = x.normal;
-						}
-					}
-				}
-				});
-		}
-
-		list.clear();
-	}
-
-	int nTex = objectVertices.size();
-	int texIndex{};
-
-	for (int f = 0; f < mesh.NF(); f++) {
-		for (int k = 0; k < 3; k++) {
-			//std::cout << mesh.F(f).v[k] << " | " << mesh.FN(f).v[k] << " | " << mesh.GetMaterialFirstFace(f) << std::endl;
-			for (int j = 0; j < nf; j++) {
-				if (mesh.F(j).v[k] == texIndex) {
-					texIndex++;
-					objectTexCoords.push_back({ mesh.VT(mesh.FT(j).v[k]).x, mesh.VT(mesh.FT(j).v[k]).y });
-					objectNormals.push_back({ mesh.VN(mesh.FN(j).v[k]).x, mesh.VN(mesh.FN(j).v[k]).y, mesh.VN(mesh.FN(j).v[k]).z });
-					//continue;
-				}
-			}
-		}
-	}
-
-	for (int i = 0; i < nf; i++) {
-		facesIndex.push_back({ mesh.F(i).v[0] });
-		facesIndex.push_back({ mesh.F(i).v[1] });
-		facesIndex.push_back({ mesh.F(i).v[2] });
-	}
-
-	std::cout << objectVertices.size() << "  <- Vertices" << std::endl;
-	std::cout << objectNormals.size() << "  <- Normals" << std::endl;
-	std::cout << objectTexCoords.size() << "  <- Tex" << std::endl;
-
-	std::cout << mesh.NF() << "  <- Faces" << std::endl;
-	std::cout << mesh.NV() << "  <- Original Vertex number" << std::endl;
-	std::cout << mesh.NVN() << "  <- Original Normal number" << std::endl;
-	std::cout << mesh.NVT() << "  <- Original Tex number" << std::endl;
-}
-
 int main(int argc, char** argv) {
 
 	glutInit(&argc, argv);
@@ -737,29 +547,12 @@ int main(int argc, char** argv) {
 		"cubemap/cubemap_posz.png",
 		"cubemap/cubemap_negz.png"
 	);
-	cy::TriMesh mesh;
-	mesh.LoadFromFileObj("teapot.obj");
 
-	std::vector<unsigned char> png;
-	std::vector<unsigned char> image; //the raw pixels
-	unsigned width{}, height{};
-	lodepng::State state; //optionally customize this one
-
-	std::cout<< "Materials: " << mesh.NM() << std::endl;
-
-	if (mesh.NM() > 0) {
-		std::cout << "MAP: " << mesh.M(0).map_Kd << std::endl;
-		
-		unsigned error = lodepng::load_file(png, std::string(mesh.M(0).map_Kd).c_str());
-		
-		if (!error) error = lodepng::decode(image, width, height, state, png);
-		if (error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
-	}
-	
 	cam.updatePosition(angleX, angleY);
 	cam.setMVP(windowHeight, windowHeight);
+	
+	object3D.loadFromFile("teapot.obj");
 
-	loadObject(mesh, objectVertices, objectNormals, objectTexCoords);
 	setObject();
 	setPlane();
 
@@ -776,7 +569,7 @@ int main(int argc, char** argv) {
 	program.Link();
 
 	//glUseProgram(program.GetID());
-	setObjectTexture(windowWidth, windowHeight, image);
+	setObjectTexture(windowWidth, windowHeight, object3D.m_image);
 	setCubemapTexConfig();
 	setUniformVariables(program.GetID());
 	setCubemapConfig();
