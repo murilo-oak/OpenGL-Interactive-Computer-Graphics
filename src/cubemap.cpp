cc080e2421222282e0bec13995bb9141b0d2b788
Murilo Carvalho <murilo63793@gmail.com>
2023-05-18 10:51:29 -0300

    refactor: cubemap setup inside class

--- src/cubemap.cpp
+++ src/cubemap.cpp
@@ -19,3 +19,39 @@ void Cubemap::loadImageFilesCubeMap(const char* posx, const char* negx, const ch
 	}
 
 }
+
+void Cubemap::setBuffer() {
+	//Cubemap
+	glGenVertexArrays(1, &m_vao);
+	glBindVertexArray(m_vao);
+
+	//positions
+	glCreateBuffers(1, &m_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+	glNamedBufferStorage(m_vbo, sizeof(m_vertices), m_vertices, 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glEnableVertexAttribArray(0);
+}
+
+void Cubemap::setTexture() {
+	glGenTextures(1, &texCubeID);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, texCubeID);
+
+	for (unsigned int i = 0; i < 6; i++)
+	{
+		glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image[i].data());
+	}
+
+	//cube filter parameters
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+}
+
+void Cubemap::set() {
+	setBuffer();
+	setTexture();
+}
--- src/cubemap.h
+++ src/cubemap.h
@@ -1,9 +1,9 @@
-#ifndef CUBEMAP_H
-#define CUBEMAP_H
+#pragma once
 
 #include <iostream>
 #include <vector>
-#include "..\lodepng.h"
+#include "../lodepng.h"
+#include "../ThirdParty/include/GL/glew.h"	
 
 class Cubemap
 {
@@ -15,7 +15,60 @@ public:
 	std::vector<unsigned char> png[6];
 	std::vector<unsigned char> image[6];
 
-	void loadImageFilesCubeMap(const char* posx, const char* negx, const char* posy, const char* negy, const char* posz, const char* negz);
-};
+	GLuint m_vao{};
+	GLuint m_vbo{};
+	GLuint texCubeID{};
 
-#endif
+	float m_vertices[108] = {
+		// positions          
+		-1.0f,  1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,
+		 1.0f, -1.0f, -1.0f,
+		 1.0f, -1.0f, -1.0f,
+		 1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
+
+		-1.0f, -1.0f,  1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,
+
+		 1.0f, -1.0f, -1.0f,
+		 1.0f, -1.0f,  1.0f,
+		 1.0f,  1.0f,  1.0f,
+		 1.0f,  1.0f,  1.0f,
+		 1.0f,  1.0f, -1.0f,
+		 1.0f, -1.0f, -1.0f,
+
+		-1.0f, -1.0f,  1.0f,
+		-1.0f,  1.0f,  1.0f,
+		 1.0f,  1.0f,  1.0f,
+		 1.0f,  1.0f,  1.0f,
+		 1.0f, -1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,
+
+		-1.0f,  1.0f, -1.0f,
+		 1.0f,  1.0f, -1.0f,
+		 1.0f,  1.0f,  1.0f,
+		 1.0f,  1.0f,  1.0f,
+		-1.0f,  1.0f,  1.0f,
+		-1.0f,  1.0f, -1.0f,
+
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f,  1.0f,
+		 1.0f, -1.0f, -1.0f,
+		 1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f,  1.0f,
+		 1.0f, -1.0f,  1.0f
+	};
+
+	Cubemap() {
+	}
+
+	void loadImageFilesCubeMap(const char* posx, const char* negx, const char* posy, const char* negy, const char* posz, const char* negz);
+	void setBuffer();
+	void setTexture();
+	void set();
+};
