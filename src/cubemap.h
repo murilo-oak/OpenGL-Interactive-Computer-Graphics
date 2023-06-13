2cdf50a30422006f0c194c4386eb5c67510b5ec5
murilo-oak <murilo63793@gmail.com>
2023-06-13 16:47:43 -0300

    docs: add function comments

--- src/cubemap.h
+++ src/cubemap.h
@@ -11,14 +11,14 @@
 class Cubemap
 {
 public:
-	unsigned int height{}, width{};
+	unsigned int m_height{}, m_width{};
 
-	std::vector<unsigned char> image[6];
-	lodepng::State state[6];
+	std::vector<unsigned char> m_images[6];
+	lodepng::State m_states[6];
 
 	GLuint m_vao{};
 	GLuint m_vbo{};
-	GLuint texCubeID{};
+	GLuint m_texCubeID{};
 
 	float m_vertices[108] = {
 		// positions          
@@ -65,14 +65,83 @@ public:
 		 1.0f, -1.0f,  1.0f
 	};
 
+	/**
+	 * @brief Default constructor for the Cubemap class.
+	 *
+	 * This constructor initializes a new instance of the Cubemap class with default values.
+	 */
 	Cubemap();
 
-	void loadImageFilesCubeMap(const char* posx, const char* negx, const char* posy, const char* negy, const char* posz, const char* negz);
-	void setBuffer();
-	void setTexture();
-	void set();
+	/**
+	 * @brief Destructor for the Cubemap class.
+	 *
+	 * This destructor releases the OpenGL resources associated with the Cubemap object, including
+	 * the vertex buffer, texture, and vertex array objects. It also clears the image data stored in
+	 * the member variable `m_images`.
+	 */
 	~Cubemap();
 
+	/**
+	 * @brief Sets up the cubemap by configuring the buffer and texture.
+	 *
+	 * This function calls the `setBuffer` and `setTexture` functions to configure the buffer
+	 * and texture for the cubemap, respectively.
+	 */
+	void set();
+
+	/**
+	 * @brief Sets up the buffers for rendering the cubemap.
+	 *
+	 * This function generates and binds the vertex array object (VAO) for the cubemap.
+	 * It creates and initializes the vertex buffer object (VBO) for storing the vertex positions.
+	 * The vertex positions are stored in the `m_vertices` array.
+	 * The vertex attribute pointer is configured to interpret the data correctly.
+	 * Finally, vertex attribute array 0 is enabled to use the vertex positions during rendering.
+	 */
+	void setBuffer();
+
+	/**
+	 * @brief Configures the texture for the cubemap using the image data in the member variable.
+	 *
+	 * Generates and binds a texture object for the cubemap using the image data stored in the member variable
+	 * `m_images`. Each face of the cubemap is specified using a separate call to `glTexImage2D`, with the format
+	 * set to GL_RGBA and the data type set to GL_UNSIGNED_BYTE. Texture parameters such as filtering, wrapping modes,
+	 * and mipmapping are configured using `glTexParameteri` and `glGenerateMipmap`.
+	 */
+	void setTexture();
+
+	/**
+	 * @brief Loads multiple image files to create a cubemap texture.
+	 *
+	 * This function loads six image files corresponding to the six faces of a cubemap texture:
+	 * positive X, negative X, positive Y, negative Y, positive Z, and negative Z.
+	 * The image files are loaded in parallel using separate threads to improve performance.
+	 * Each image is decoded into RGBA pixel data and stored in the respective element of the 'image' vector.
+	 * The LodePNG state object for each image is stored in the respective element of the 'state' vector.
+	 *
+	 * @param posx The file path of the positive X face image.
+	 * @param negx The file path of the negative X face image.
+	 * @param posy The file path of the positive Y face image.
+	 * @param negy The file path of the negative Y face image.
+	 * @param posz The file path of the positive Z face image.
+	 * @param negz The file path of the negative Z face image.
+	 */
+	void loadImageFilesCubeMap(const char* posx, const char* negx, const char* posy, const char* negy, const char* posz, const char* negz);
+
 private:
+	/**
+	 * @brief Loads an image from the specified file path and decodes it into RGBA pixel data.
+	 *
+	 * This function loads an image from the provided file path using the LodePNG library.
+	 * It decodes the image into RGBA pixel data and stores it in the 'image' vector.
+	 * The width and height of the loaded image are also returned through the 'width' and 'height' parameters.
+	 * If any error occurs during the decoding process, an error message is printed to the console.
+	 *
+	 * @param imagePath The file path of the image to load.
+	 * @param image The vector to store the RGBA pixel data of the loaded image.
+	 * @param width The width of the loaded image (output parameter).
+	 * @param height The height of the loaded image (output parameter).
+	 * @param state The LodePNG state object used for decoding the image.
+	 */
 	void loadImage(const char* imagePath, std::vector<unsigned char>& image, unsigned int& width, unsigned int& height, lodepng::State state);
 };
--- src/scene.h
+++ src/scene.h
@@ -18,14 +18,83 @@ class IScene
 public:
 	IScene(){}
 
+	/**
+	 * @brief Sets up the initial configuration for the derived class.
+	 *
+	 * This pure virtual function must be implemented by the derived class to perform the necessary setup tasks.
+	 * It takes the window width and height as parameters.
+	 *
+	 * @param windowWidth The width of the window.
+	 * @param windowHeight The height of the window.
+	 */
 	virtual void setup(unsigned int windowWidth, unsigned int windowHeight) = 0;
+	
+	/**
+	 * @brief Reshapes the window with the specified width and height.
+	 *
+	 * This pure virtual function must be implemented by the derived class to handle the window reshaping logic.
+	 * It takes the new window width and height as parameters.
+	 *
+	 * @param windowWidth The new width of the window.
+	 * @param windowHeight The new height of the window.
+	 */
 	virtual void reshapeWindow(unsigned int windowWidth, unsigned int windowHeight) = 0;
+	
+	/**
+	 * @brief Updates the scene state.
+	 *
+	 * This pure virtual function must be implemented by the derived class to update the state of the scene.
+	 * It should handle any necessary updates or calculations for the scene's logic and behavior.
+	 */
 	virtual void update() = 0;
+
+	/**
+	 * @brief Renders the scene.
+	 *
+	 * This pure virtual function must be implemented by the derived class to render the scene.
+	 * It should handle all the necessary rendering operations to display the scene on the screen.
+	 */
 	virtual void render() = 0;
 
+	/**
+	 * @brief Event handler for right mouse button.
+	 *
+	 * This is a pure virtual function that serves as an event handler for the right mouse button.
+	 * It takes a `MouseInput` object as a parameter, which provides information about the mouse input.
+	 * Subclasses of the `Scene` class must implement this function to define their own behavior for handling right mouse button events.
+	 *
+	 * @param mouse The `MouseInput` object containing information about the mouse input.
+	 */
 	virtual void onRightButton(MouseInput mouse) = 0;
+
+	/**
+	 * @brief Event handler for left mouse button.
+	 *
+	 * This is a pure virtual function that serves as an event handler for the left mouse button.
+	 * It takes a `MouseInput` object as a parameter, which provides information about the mouse input.
+	 * Subclasses of the `Scene` class must implement this function to define their own behavior for handling left mouse button events.
+	 *
+	 * @param mouse The `MouseInput` object containing information about the mouse input.
+	 */
 	virtual void onLeftButton(MouseInput mouse) = 0;
+
+	/**
+	 * @brief Event handler for secondary left mouse button.
+	 *
+	 * This is a pure virtual function that serves as an event handler for the secondary (or additional) left mouse button.
+	 * It takes a `MouseInput` object as a parameter, which provides information about the mouse input.
+	 * Subclasses of the `Scene` class must implement this function to define their own behavior for handling the secondary left mouse button events.
+	 *
+	 * @param mouse The `MouseInput` object containing information about the mouse input.
+	 */
 	virtual void onLeftButton2(MouseInput mouse) = 0;
+
+	/**
+	 * @brief Recompiles the shaders used by the derived class.
+	 *
+	 * This is a pure virtual function that serves to recompile the shaders used by the derived class.
+	 * Subclasses of the `Scene` class must implement this function to define their own shader recompilation logic.
+	 */
 	virtual void recompileShaders() = 0;
 };
 
